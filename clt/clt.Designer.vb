adba2493ba72c1dff06f04489640396d72ebcff7
JoomlaBoat <ivankomlev@gmail.com>
2020-05-21 21:30:29 -0500

    Initial commit

---
+++ clt/clt.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class clt
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.OK_Button = New System.Windows.Forms.Button()
+        Me.Cancel_Button = New System.Windows.Forms.Button()
+        Me.inputFile = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.outputFile = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.findstart = New System.Windows.Forms.TextBox()
+        Me.findend = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SearchOffset = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(239, 130)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.TabIndex = 5
+        Me.OK_Button.Text = "OK"
+        '
+        'Cancel_Button
+        '
+        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+        Me.Cancel_Button.Name = "Cancel_Button"
+        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.TabIndex = 1
+        Me.Cancel_Button.Text = "Cancel"
+        '
+        'inputFile
+        '
+        Me.inputFile.Location = New System.Drawing.Point(71, 6)
+        Me.inputFile.Name = "inputFile"
+        Me.inputFile.Size = New System.Drawing.Size(238, 20)
+        Me.inputFile.TabIndex = 1
+        Me.inputFile.Text = "C:\sql"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 94)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Output File:"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(327, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(58, 26)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Open"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Find Start Text:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(76, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Find End Text:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Input File:"
+        '
+        'outputFile
+        '
+        Me.outputFile.Location = New System.Drawing.Point(71, 91)
+        Me.outputFile.Name = "outputFile"
+        Me.outputFile.Size = New System.Drawing.Size(238, 20)
+        Me.outputFile.TabIndex = 4
+        Me.outputFile.Text = "C:\test"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(327, 87)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(58, 26)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'findstart
+        '
+        Me.findstart.Location = New System.Drawing.Point(100, 32)
+        Me.findstart.Name = "findstart"
+        Me.findstart.Size = New System.Drawing.Size(209, 20)
+        Me.findstart.TabIndex = 2
+        Me.findstart.Text = "INSERT INTO stud_details"
+        '
+        'findend
+        '
+        Me.findend.Location = New System.Drawing.Point(100, 59)
+        Me.findend.Name = "findend"
+        Me.findend.Size = New System.Drawing.Size(209, 20)
+        Me.findend.TabIndex = 3
+        Me.findend.Text = "INSERT INTO stud_emrg_call"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(19, 134)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(72, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Start Position:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(19, 149)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(69, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "End Position:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(328, 35)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(38, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Offset:"
+        '
+        'SearchOffset
+        '
+        Me.SearchOffset.Location = New System.Drawing.Point(331, 51)
+        Me.SearchOffset.Name = "SearchOffset"
+        Me.SearchOffset.Size = New System.Drawing.Size(53, 20)
+        Me.SearchOffset.TabIndex = 8
+        Me.SearchOffset.Text = "1000000000"
+        '
+        'clt
+        '
+        Me.AcceptButton = Me.OK_Button
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Cancel_Button
+        Me.ClientSize = New System.Drawing.Size(397, 171)
+        Me.Controls.Add(Me.SearchOffset)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.findend)
+        Me.Controls.Add(Me.findstart)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.outputFile)
+        Me.Controls.Add(Me.inputFile)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "clt"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Copy Large Text"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents Cancel_Button As System.Windows.Forms.Button
+    Friend WithEvents inputFile As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents outputFile As System.Windows.Forms.TextBox
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents findstart As System.Windows.Forms.TextBox
+    Friend WithEvents findend As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents SearchOffset As System.Windows.Forms.TextBox
+
+End Class
---
+++ clt/clt.vb
@@ -0,0 +1,173 @@
+﻿Imports System.Windows.Forms
+
+Public Class clt
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Dim a As String
+        a = inputFile.Text
+        Dim File1 = IO.File.Open(a, IO.FileMode.Open, IO.FileAccess.Read)
+
+        Dim bufferSize As Long = 100000
+        Dim findwhat1 As Byte()
+        findwhat1 = System.Text.Encoding.Unicode.GetBytes(findstart.Text)
+        'findwhat1 = System.Text.Encoding.ASCII.GetBytes(findstart.Text)
+
+        Dim findwhat2 As Byte()
+        findwhat2 = System.Text.Encoding.Unicode.GetBytes(findend.Text)
+        'findwhat2 = System.Text.Encoding.ASCII.GetBytes(findend.Text)
+
+
+        Dim writeBuffer = New Byte(10000) {}
+        Dim wp = 0
+
+        Dim bytes = New Byte(bufferSize - 1) {}
+        Dim p1 As Long = 0
+        Dim pl1 As Long = findwhat1.Length
+
+        Dim p2 As Long = 0
+        Dim pl2 As Long = findwhat2.Length
+
+        Dim offset As Long = 0
+        Dim start As Long = -1
+        Dim endpos As Long = -1
+        Dim i As Long
+        Dim sOffset As Long = Val(SearchOffset.Text)
+
+        While File1.Read(bytes, 0, bufferSize) > 0
+
+            If endpos = -1 Then
+                Label6.Text = "Offset: " + offset.ToString
+                System.Windows.Forms.Application.DoEvents()
+            End If
+
+            If offset >= sOffset Then
+
+                For i = 0 To bufferSize - 1
+
+                    If start = -1 Then
+                        If bytes(i) = findwhat1(p1) Then
+                            p1 = p1 + 1
+                            If p1 = pl1 Then
+                                'found
+                                start = i + offset - pl1 + 1
+                                Label5.Text = "Found start at: " + start.ToString
+                                writeBuffer = findwhat1
+                                wp = pl1 - 1
+                                p1 = 0
+
+                            End If
+                            
+                        Else
+                            p1 = 0
+                        End If
+                    End If
+                    If start > -1 Then
+                        Dim k As Long = endpos - start
+                        If endpos > -1 Then
+                            If wp >= k Then
+                                Exit While
+                            End If
+                        End If
+
+
+                        If wp >= writeBuffer.Length Then
+                            ReDim Preserve writeBuffer(writeBuffer.Length + bufferSize)
+                        End If
+                        writeBuffer(wp) = bytes(i) ' - pl1 + 1)
+                        wp = wp + 1
+
+
+
+                    End If
+
+                    If start > -1 And endpos = -1 Then
+                        If bytes(i) = findwhat2(p2) Then
+                            p2 = p2 + 1
+                            If p2 = pl2 Then
+                                'found end
+                                endpos = i + offset + 1
+                                Label6.Text = "Found end at: " + endpos.ToString
+
+                            End If
+
+                            If p2 = pl2 Then
+                                p2 = 0
+                            End If
+                        Else
+                            p2 = 0
+                        End If
+                    Else
+
+
+                    End If
+                Next
+
+            End If
+            'If p = pl Then
+            ' Exit While
+            ' End If
+
+
+
+
+            offset = offset + bufferSize
+        End While
+
+
+
+        File1.Close()
+
+        If start > -1 And endpos > -1 Then
+            'Save file
+
+
+            If System.IO.File.Exists(outputFile.Text) = True Then
+
+                System.IO.File.Delete(outputFile.Text)
+            End If
+
+
+            Dim File2 = IO.File.Open(outputFile.Text, IO.FileMode.CreateNew, IO.FileAccess.Write)
+
+            File2.Write(writeBuffer, 0, wp)
+
+
+            File2.Close()
+
+        End If
+
+    End Sub
+
+
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        inputFile.Text = OpenFileDialog1.FileName
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        SaveFileDialog1.ShowDialog()
+        outputFile.Text = SaveFileDialog1.FileName
+
+
+    End Sub
+
+
+
+
+    Private Sub clt_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles findstart.TextChanged, findend.TextChanged
+
+    End Sub
+End Class
